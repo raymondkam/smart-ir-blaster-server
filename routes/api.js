a0068f66dc872a7f56967d403809c5d267e2399c
Raymond Kam <raymondhmkam@gmail.com>
2017-12-10 17:27:59 -0500

    Use ssl with websocket server, moved websocket server from api to www.

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const apiConfig = require('../config/api.json');
-const WebSocket = require('ws');
-const wsConfig = require('../config/websocket.json');
 
-const wss = new WebSocket.Server({ port: 8081 });
-
-let wsClient = null
+let wsClient = null;
 
 // auth middleware
 const authMiddleware = (req, res, next) => {
@@ -35,7 +31,6 @@ router.get('/health', (req, res, next) => {
 router.post('/tv', (req, res, next) => {
     let json = req.body;
     if (json != null) {
-        console.log('json data: ', json);
         if (wsClient) {
             wsClient.send(JSON.stringify(json));
         }
@@ -45,34 +40,9 @@ router.post('/tv', (req, res, next) => {
     }
 })
 
-// ------------------
-// WEBSOCKET
-// ------------------
-wss.on('connection', function connection(ws, req) {
-    console.log('req: ', req);
-
-    // TODO: check to see if it is the ip of the raspberry pi/your home public ip
-    const ip = req.connection.remoteAddress;
-    console.log("ip address 1: ", req.ip);
-
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-        const messageJSON = JSON.parse(message);
-        if (messageJSON.token === wsConfig.token) {
-            wsClient = ws;
-
-            wsClient.on('message', function incoming(message) {
-                console.log("received subsequent message from trusted client: " + message);
-            });
-            wsClient.on('close', function close() {
-                console.log('websocket connection closed');
-                wsClient = null
-            });
-        } else {
-            console.log('failed to auth, close connection');
-            ws.close(1003, "Missing or bad token");
-        }
-    });
-});
-
-module.exports = router;
+module.exports = {
+    'router': router,
+    'setWsClient': (newWsClient) => {
+        wsClient = newWsClient;
+    }
+}
