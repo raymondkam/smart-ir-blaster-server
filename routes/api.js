19e5a80345db230168bb4471fe9f0e6ca8bdd912
Raymond Kam <raymondhmkam@gmail.com>
2017-12-10 13:39:13 -0500

    Added Websocket library to project and connected it to the /api/tv route. Added self signed cert for HTTPS.

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,52 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const WebSocket = require('ws');
+const wsConfig = require('../config/websocket.json');
 
-router.get('/', (req, res, next) => {
+const wss = new WebSocket.Server({ port: 8081 });
+
+let client = null
+
+wss.on('connection', function connection(ws, req) {
+    console.log('req: ', req);
+    const ip = req.connection.remoteAddress;
+    console.log("ip address 1: ", req.ip);
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        const messageJSON = JSON.parse(message);
+        if (messageJSON.token === wsConfig.token) {
+            client = ws;
+
+            client.on('message', function incoming(message) {
+                console.log("received subsequent message from trusted client: " + message);
+            });
+            client.on('close', function close() {
+                console.log('websocket connection closed');
+                client = null
+            });
+        } else {
+            console.log('failed to auth, close connection');
+            ws.close(1003, "Missing or bad token");
+        }
+    });
+});
+
+router.get('/health', (req, res, next) => {
     res.send(200);
 })
 
-router.get('/tv', (req, res, next) => {
-    res.status(200).send('asdf');
-})
-
 router.post('/tv', (req, res, next) => {
-    res.status(200).send('asdf');
+    let json = req.body;
+    if (json != null) {
+        console.log('json data: ', json);
+        if (client) {
+            client.send(JSON.stringify(json));
+        }
+        res.status(200).send();
+    } else {
+        res.status(400).send();
+    }
 })
 
 module.exports = router;
